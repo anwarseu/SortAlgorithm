894be6c8900a9c855dc0a00eb20e89c47d453276
Md Anwar Hossain <anwarseu@gmail.com>
2019-08-15 18:57:32 +0600

    Create SelectionSort.c

---
+++ SelectionSort.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+void binary_search(int A[], int n){
+    int index_min;
+    for (int i = 0; i < n; i++) {
+        index_min = i;
+        for (int j = i + 1; j < n; j++) {
+            if(A[j] < A[index_min]){
+                index_min = j;
+            }
+        }
+        if (index_min != i) {
+            int temp = A[i];
+            A[i] = A[index_min];
+            A[index_min] = temp;
+        }
+    }
+}
+
+int main(int argc, const char * argv[]) {
+    int A[] = {3, 1, 40, 20, 10, 20, 21, 13, 14, 18};
+    binary_search(A, 10);
+    for (int i = 0; i < 10; i++) {
+        printf("%d ", A[i]);
+    }
+    printf("\n");
+    return 0;
+}
